5a46cfab1bc579a4878a8f560288e8c5cdf40ec9
admin <admin@DESKTOP-SMCC74R>
2026-03-11 15:35:12 +0530

    operators

---
+++ Operatorsprogram/src/operators/OperatorsExample.java
@@ -0,0 +1,36 @@
+package operators;
+
+public class OperatorsExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 10;
+	        int b = 5;
+
+	        // Arithmetic Operators
+	        System.out.println("Arithmetic Operators:");
+	        System.out.println("a + b = " + (a + b));
+	        System.out.println("a - b = " + (a - b));
+	        System.out.println("a * b = " + (a * b));
+	        System.out.println("a / b = " + (a / b));
+	        System.out.println("a % b = " + (a % b));
+
+	        // Relational Operators
+	        System.out.println("\nRelational Operators:");
+	        System.out.println("a > b = " + (a > b));
+	        System.out.println("a < b = " + (a < b));
+	        System.out.println("a == b = " + (a == b));
+	        System.out.println("a != b = " + (a != b));
+
+	        // Logical Operators
+	        System.out.println("\nLogical Operators:");
+	        boolean x = true;
+	        boolean y = false;
+
+	        System.out.println("x && y = " + (x && y));
+	        System.out.println("x || y = " + (x || y));
+	        System.out.println("!x = " + (!x));
+
+	}
+
+}
