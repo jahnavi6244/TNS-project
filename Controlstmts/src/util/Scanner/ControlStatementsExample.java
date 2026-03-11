b32c280b802e0a9bd37c59466ffbdd6507b4338f
admin <admin@DESKTOP-SMCC74R>
2026-03-11 15:32:18 +0530

    Control statements

---
+++ Controlstmts/src/util/Scanner/ControlStatementsExample.java
@@ -0,0 +1,24 @@
+package util.Scanner;
+
+public class ControlStatementsExample {
+	public static void main(String[] args) {
+		//Forloop
+		for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+		//While loop
+	    int i = 1;
+
+        while(i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+        //d0 while
+     
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 5);
+
+}
+}
